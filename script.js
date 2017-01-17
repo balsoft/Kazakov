81823fcc92a1c9d3aaadbb5364278d1f4eae2216
privalovk <Privalov.Kol@yandex.ru>
2017-01-17 19:29:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 (function()
  {
-  var s= document.GetElementsByClassName("column-middle"),
+  var s= document.getElementsByClass('column-middle');
   style = a[0].stile;
   stile.height="1280px";
 
 
 
-})
+})();
