53bf6cbdb24601e1ea9d4dba793f377e36d9e6b4
privalovk <Privalov.Kol@yandex.ru>
2017-01-17 19:33:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 (function()
  {
   var s= document.getElementsByClass('column-middle');
-  style = a[0].stile;
-  stile.height="1280px";
+  style=.column-middle[0].stile;
+  style.height="1280px";
 
 
 
