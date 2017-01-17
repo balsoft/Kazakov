d78f0c6752527a16ac201a643296316404840ba6
privalovk <Privalov.Kol@yandex.ru>
2017-01-17 20:40:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var a = document.getElementsByClassName('column-middle'),
-n1=a[0].style,
-n2=a[1].style;
+  h=document.getElementById('wrapper'),
+  hs=h.getBoundingClientRect(),
+  hheight = hs.bottom - hs.top,
+  n1=a[0].style,
+  n2=a[1].style;
 
-n1.height="1280px";
-n2.height="1280px";
+n1.height=hheight;
+n2.height=hheight;
