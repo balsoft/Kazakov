9804f79b9dcf52c1608449b22fc7399ee8846ca8
privalovk <Privalov.Kol@yandex.ru>
2017-01-17 20:53:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var a = document.getElementsByClassName('column-middle'),
-  h=document.getElementById('wrapper'),
-  hs=h.getBoundingClientRect(),
-  hheight = hs.bottom - hs.top,
-  n1=a[0].style,
-  n2=a[1].style;
+var a = document.getElementsByClassName('column-middle');
+var  h=document.getElementById('wrapper');
+var  hs=h.getBoundingClientRect();
+var  hheight = hs.bottom - hs.top;
+var  n1=a[0].style;
+var  n2=a[1].style;
 
 n1.height=hheight;
 n2.height=hheight;
