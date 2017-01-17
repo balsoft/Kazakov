a5e422df7a8ffe3ef0e17f80c199ce3cdd46f9e9
privalovk <Privalov.Kol@yandex.ru>
2017-01-17 18:29:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-var y=473;
-function add()
-{
-document.getElementById('orn4').style.height = y + 25 + 'px' ;
-    y = y + 25;
-}
-nb_2.onclick=function()
-    {
-        document.getElementById('orn4').style.height = y - 25 + 'px' ;
-        y = y - 25;
- 
-    }
+
