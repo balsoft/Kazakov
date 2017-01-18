7ed5df7214724917825652d1d6b59ce323b20147
privalovk <Privalov.Kol@yandex.ru>
2017-01-18 21:22:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var a = document.getElementsByClassName('column-middle');
-var  h=document.getElementById('wrapper');
+var h = document.getElementById('wrapper');
 
 var  n1=a[0].style;
 var  n2=a[1].style;
-
-n1.height=h.style.height;
-n2.height=h.style.height;
+var Hei=h.style;
+n1.height=Hei.height;
+n2.height=Hei.height;
