cf47cefae89db2b64bc6fce669c13a96d7431992
Александр Бантьев <balsoft@yandex.ru>
2017-01-30 15:35:31 +0300

    Форматировал .js

--- resizeColumns.js
+++ resizeColumns.js
@@ -1,4 +1,4 @@
-setInterval(()=>{
-  document.querySelectorAll(".column")[0].style.height = document.getElementById("wrapper").getClientRects()[0].height + 50 + "px"
-  document.querySelectorAll(".column")[1].style.height = document.getElementById("wrapper").getClientRects()[0].height + 50 + "px"
+setInterval(() => {
+	document.querySelectorAll(".column")[0].style.height = document.getElementById("wrapper").getClientRects()[0].height + 50 + "px"
+	document.querySelectorAll(".column")[1].style.height = document.getElementById("wrapper").getClientRects()[0].height + 50 + "px"
 }, 100);
