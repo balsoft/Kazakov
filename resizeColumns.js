92f9c170708f5dadca42d939f347a122e8831c35
Александр Бантьев <balsoft@yandex.ru>
2017-01-31 19:43:42 +0300

    Заметка

--- resizeColumns.js
+++ resizeColumns.js
@@ -1,3 +1,6 @@
+/* 
+** THIS SOFTWARE COMES "AS IS", WITHOUT ANY WARRANTY TO THE EXTENT PERMITTED BY APPLICABLE LAW. 
+*/
 setInterval(() => {
 	document.querySelectorAll(".column")[0].style.height = document.getElementById("wrapper").getClientRects()[0].height + 50 + "px"
 	document.querySelectorAll(".column")[1].style.height = document.getElementById("wrapper").getClientRects()[0].height + 50 + "px"
